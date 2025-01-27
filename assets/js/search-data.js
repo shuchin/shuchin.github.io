5fc10f0c5efb12a5a444d1aaedb5b5c60ce61e66
shuchin <shuchin@ece.tufts.edu>
2025-01-27 22:10:08 +0000

    Deploying to gh-pages from @ shuchin/shuchin.github.io@d0b9ba05bc36cf42b21907b576462320c869a46f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,16 +431,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-two-papers-accepted-to-aistats-2025",
+          title: 'Two papers accepted to AISTATS 2025',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',

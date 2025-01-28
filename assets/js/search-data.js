2d4d0838077563282141b31a3c117c62426c1bcc
shuchin <shuchin@ece.tufts.edu>
2025-01-28 14:37:41 +0000

    Deploying to gh-pages from @ shuchin/shuchin.github.io@42c8887b3e9e78359427fd6ad82c3ca7d44c66a3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "For a full pdf CV download the pdf from top left.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";

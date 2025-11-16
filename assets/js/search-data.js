c189bb4b89a8bec7479b8a4ce67ddfc2179562ce
shuchin <shuchin@ece.tufts.edu>
2025-11-16 22:16:14 +0000

    Deploying to gh-pages from @ shuchin/shuchin.github.io@803e29d703c7f4be8a0828cb531196607ba04d66 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "Students, Post-Docs, and Alumni",
+          description: "Current and Alumni",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";

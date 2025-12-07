2f5e7fee1fb1a9f8725ad1a0cec220647abd0677
shuchin <shuchin@ece.tufts.edu>
2025-12-07 19:15:46 +0000

    Deploying to gh-pages from @ shuchin/shuchin.github.io@16985d27e10663600421586a5255344b73abe52f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -446,6 +446,13 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
+        id: 'social-dblp',
+        title: 'DBLP',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://dblp.org/pid/14/6374.html", "_blank");
+        },
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
